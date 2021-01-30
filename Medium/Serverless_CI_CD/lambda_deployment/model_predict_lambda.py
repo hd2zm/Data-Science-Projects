4b853ee891f9ec5ab4caf0a3914a433c2a907c16
Hari Devanathan <hd2zm@virginia.edu>
2021-01-30 18:00:24 -0500

    print result

--- Medium/Serverless_CI_CD/lambda_deployment/model_predict_lambda.py
+++ Medium/Serverless_CI_CD/lambda_deployment/model_predict_lambda.py
@@ -16,20 +16,25 @@ import sys
 def model_predict_handler(event, context):
 
     # The SageMaker runtime is what allows us to invoke the endpoint that we've created.
-    runtime = boto3.Session().client('sagemaker-runtime')
+    runtime = boto3.Session().client('sagemaker-runtime')                     
 
-    # Now we use the SageMaker runtime to invoke our endpoint, sending the review we were given
-    response = runtime.invoke_endpoint(EndpointName = 'sagemaker-role',    # The name of the endpoint we created
+    for record in event['Records']:
+        
+        record_body = json.loads(record['body')
+
+        # Now we use the SageMaker runtime to invoke our endpoint, sending the review we were given
+        response = runtime.invoke_endpoint(EndpointName = 'sagemaker-role',    # The name of the endpoint we created
                                        ContentType = 'text/plain',                 # The data format that is expected
-                                       Body = event['body'])                       # The actual review
+                                       Body = record_body)  
+        # The response is an HTTP response whose body contains the result of our inference
+        result = response['Body'].read().decode('utf-8')
 
-    # The response is an HTTP response whose body contains the result of our inference
-    result = response['Body'].read().decode('utf-8')
+        print(result)
 
-    return {
-        'statusCode' : 200,
-        'headers' : { 'Content-Type' : 'text/plain', 'Access-Control-Allow-Origin' : '*' },
-        'body' : result
-    }
+        return {
+            'statusCode' : 200,
+            'headers' : { 'Content-Type' : 'text/plain', 'Access-Control-Allow-Origin' : '*' },
+            'body' : result
+        }
 
     
